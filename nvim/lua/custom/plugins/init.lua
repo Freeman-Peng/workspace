9d673deb4093bb6e9f93e91156633cbbac3f47b2
Freeman <freeman@joytrip.org>
2022-12-09 15:00:57 +0800

    feat:add nvim

---
+++ nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,28 @@
+
+local M = {
+    -- Override plugin definition options
+  ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+  },
+
+  -- null-ls
+  ["jose-elias-alvarez/null-ls.nvim"] = {
+    after = "nvim-lspconfig",
+    config = function()
+      require "custom.plugins.null-ls"
+    end,
+  },
+
+  -- surround
+  ["kylechui/nvim-surround"] = {
+    tag = "*",
+    config = function ()
+      require("nvim-surround").setup{}
+    end
+  },
+}
+
+return M
