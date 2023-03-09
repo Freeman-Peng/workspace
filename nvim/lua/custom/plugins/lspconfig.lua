6dc9447b51b06fcadd8150fab8956cf34d6afdde
freeman.peng <freeman_peng@163.com>
2023-03-10 04:00:17 +0800

    feat: add cmake and nvim-tree ignore list

--- nvim/lua/custom/plugins/lspconfig.lua
+++ nvim/lua/custom/plugins/lspconfig.lua
@@ -1,24 +1,24 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
-local servers = { "gopls", "html", "cssls", "tsserver", "volar", "pylsp" }
+local servers = { "gopls", "html", "cssls", "tsserver", "volar", "pylsp", "cmake" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
-lspconfig["clangd"].setup {
-  cmd = {
-    "clangd",
-    "--clang-tidy",
-    "--completion-style=bundled",
-    "--header-insertion=iwyu",
-  },
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
+lspconfig["clangd"].setup({
+	cmd = {
+		"clangd",
+		"--clang-tidy",
+		"--completion-style=bundled",
+		"--header-insertion=iwyu",
+	},
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
