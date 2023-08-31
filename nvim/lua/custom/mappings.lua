d7afc241900a48e9dee5a9e42e82e103d4c740b1
Freeman <freeman@jytrp.com>
2023-08-31 12:27:19 +0800

    feat: add support jump to end of context

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -44,4 +44,24 @@ M.general = {
   },
 }
 
+M.blankline = {
+  n = {
+    ["<leader>ce"] = {
+      function()
+        local ok, _, endp = require("indent_blankline.utils").get_current_context(
+          vim.g.indent_blankline_context_patterns,
+          vim.g.indent_blankline_use_treesitter_scope
+        )
+
+        if ok then
+          vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { endp, 0 })
+          vim.cmd [[normal! _]]
+        end
+      end,
+
+      "Jump to current context",
+    },
+  },
+}
+
 return M
