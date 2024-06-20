f028bd7c5879732bdc83427febe14eb6a56d2c5e
Freeman <freeman@jytrp.com>
2024-06-20 18:34:32 +0800

    feat: change default c,cpp commentstring

--- lua/options.lua
+++ lua/options.lua
@@ -5,8 +5,14 @@ require "nvchad.options"
 local o = vim.o
 
 -- o.cursorlineopt ='both' -- to enable cursorline!
-o.mouse=""
+o.mouse = ""
 
-
--- autocmds 
+-- autocmds
 -- local custom_auto_group = vim.api.nvim_create_augroup("custom_grp", { clear = false })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "c", "cpp" },
+  callback = function()
+    vim.bo.commentstring = "// %s"
+  end,
+})
