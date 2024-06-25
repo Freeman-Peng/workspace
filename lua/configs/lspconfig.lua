0fc32eb1fccfe6120310d35392825b440b7b971c
Freeman <freeman@jytrp.com>
2024-06-25 19:40:49 +0800

    feat: add emmit lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,20 +3,21 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "lua_ls", "html", "cssls", "tsserver", "volar", "pylsp", "cmake", "gopls", "clangd", "jdtls"}
+local servers =
+  { "lua_ls", "html", "cssls", "tsserver", "volar", "pylsp", "cmake", "gopls", "clangd", "jdtls", "emmet_ls" }
 local map = vim.keymap.set
 
 local custom_map = function()
-      map("n", "<F1>", ":Telescope lsp_document_symbols<CR>")
-      map("n", "]d", vim.diagnostic.goto_next)
-      map("n", "[d", vim.diagnostic.goto_prev)
-      map("n", "<leader>lf", vim.diagnostic.open_float)
+  map("n", "<F1>", ":Telescope lsp_document_symbols<CR>")
+  map("n", "]d", vim.diagnostic.goto_next)
+  map("n", "[d", vim.diagnostic.goto_prev)
+  map("n", "<leader>lf", vim.diagnostic.open_float)
 end
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = function (client, bufnr)
+    on_attach = function(client, bufnr)
       custom_map()
       on_attach(client, bufnr)
     end,
@@ -25,14 +26,14 @@ for _, lsp in ipairs(servers) do
   }
 end
 
-require('lspconfig').jsonls.setup {
+require("lspconfig").jsonls.setup {
   settings = {
     json = {
-      schemas = require('schemastore').json.schemas(),
+      schemas = require("schemastore").json.schemas(),
       validate = { enable = true },
     },
   },
-  on_attach = function (client, bufnr)
+  on_attach = function(client, bufnr)
     custom_map()
     on_attach(client, bufnr)
   end,
