3251e1b79d8df62656354c608b4549d3980074ad
Freeman <freeman@jytrp.com>
2024-08-01 20:35:12 +0800

    add auto backup

--- lua/options.lua
+++ lua/options.lua
@@ -6,6 +6,22 @@ local o = vim.o
 
 -- o.cursorlineopt ='both' -- to enable cursorline!
 o.mouse = ""
+o.backup = true
+o.backupskip = ""
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  callback = function()
+    local path = vim.fn.expand "%:p:h"
+    if path == "" then
+      path = vim.fn.getcwd()
+    end
+    local full_path = vim.fn.stdpath "state" .. "/backup" .. path
+    if not vim.fn.filereadable(full_path) then
+      vim.fn.mkdir(full_path, "pR")
+    end
+    o.backupdir = full_path
+  end,
+})
 
 -- autocmds
 -- local custom_auto_group = vim.api.nvim_create_augroup("custom_grp", { clear = false })
