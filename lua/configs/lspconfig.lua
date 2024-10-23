f334307f28b471ad583e3b1b01a7af28f211442a
freeman.peng <freeman_peng@163.com>
2024-10-23 22:51:12 +0800

    add lsp for emmet, tailwindcss

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,111 +2,112 @@ local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 local map = vim.keymap.set
 
 local custom_map = function(bufnr)
-  local function opts(desc)
-    return { buffer = bufnr, desc = "LSP " .. desc }
-  end
-  map("n", "<F1>", ":Telescope lsp_document_symbols<CR>", opts "show current symbols")
-  map("n", "<leader>ds", ":Telescope lsp_document_symbols<CR>", opts "show current symbols")
-  map("n", "<leader>q", ":Telescope diagnostics<CR>", opts "show diagnostics window")
-  map("n", "]d", vim.diagnostic.goto_next, opts "goto prev dignositc")
-  map("n", "[d", vim.diagnostic.goto_prev, opts "goto prev dignositc")
-  map("n", "<leader>lf", vim.diagnostic.open_float, opts "show float diagnostic")
-  map("n", "gr", ":Telescope lsp_references<CR>", opts "show reference")
-  map("n", "gD", vim.lsp.buf.declaration, opts "Go to declaration")
-  map("n", "gd", ":Telescope lsp_definitions<CR>", opts "Go to declaration")
+	local function opts(desc)
+		return { buffer = bufnr, desc = "LSP " .. desc }
+	end
+	map("n", "<F1>", ":Telescope lsp_document_symbols<CR>", opts("show current symbols"))
+	map("n", "<leader>ds", ":Telescope lsp_document_symbols<CR>", opts("show current symbols"))
+	map("n", "<leader>q", ":Telescope diagnostics<CR>", opts("show diagnostics window"))
+	map("n", "]d", vim.diagnostic.goto_next, opts("goto prev dignositc"))
+	map("n", "[d", vim.diagnostic.goto_prev, opts("goto prev dignositc"))
+	map("n", "<leader>lf", vim.diagnostic.open_float, opts("show float diagnostic"))
+	map("n", "gr", ":Telescope lsp_references<CR>", opts("show reference"))
+	map("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
+	map("n", "gd", ":Telescope lsp_definitions<CR>", opts("Go to declaration"))
 end
 
 local settings = {
-  "lua_ls",
-  "html",
-  "cssls",
-  "cmake",
-  "gopls",
-  "jdtls",
-  "rust_analyzer",
-  "emmet_ls",
-  "volar",
-  tsserver = {
-    init_options = {
-      plugins = {
-        {
-          name = "@vue/typescript-plugin",
-          location = require("mason-registry").get_package("vue-language-server"):get_install_path()
-            .. "/node_modules/@vue/language-server",
-          languages = { "vue" },
-        },
-      },
-    },
-    filetypes = {
-      "javascript",
-      "javascriptreact",
-      "javascript.jsx",
-      "typescript",
-      "typescriptreact",
-      "typescript.tsx",
-      "vue",
-    },
-  },
-  pylsp = {
-    settings = {
-      pylsp = {
-        plugins = {
-          pycodestyle = {
-            enable = true,
-            maxLineLength = 120,
-          },
-        },
-      },
-    },
-  },
-  jsonls = {
-    settings = {
-      json = {
-        schemas = require("schemastore").json.schemas(),
-        validate = { enable = true },
-      },
-    },
-  },
-  clangd = {
-    cmd = {
-      "clangd",
-      "--completion-style=detailed",
-      "--pch-storage=memory",
-    },
-  },
+	"lua_ls",
+	"html",
+	"cssls",
+	"cmake",
+	"gopls",
+	"jdtls",
+	"rust_analyzer",
+	"emmet_ls",
+	"volar",
+	"tailwindcss",
+	tsserver = {
+		init_options = {
+			plugins = {
+				{
+					name = "@vue/typescript-plugin",
+					location = require("mason-registry").get_package("vue-language-server"):get_install_path()
+						.. "/node_modules/@vue/language-server",
+					languages = { "vue" },
+				},
+			},
+		},
+		filetypes = {
+			"javascript",
+			"javascriptreact",
+			"javascript.jsx",
+			"typescript",
+			"typescriptreact",
+			"typescript.tsx",
+			"vue",
+		},
+	},
+	pylsp = {
+		settings = {
+			pylsp = {
+				plugins = {
+					pycodestyle = {
+						enable = true,
+						maxLineLength = 120,
+					},
+				},
+			},
+		},
+	},
+	jsonls = {
+		settings = {
+			json = {
+				schemas = require("schemastore").json.schemas(),
+				validate = { enable = true },
+			},
+		},
+	},
+	clangd = {
+		cmd = {
+			"clangd",
+			"--completion-style=detailed",
+			"--pch-storage=memory",
+		},
+	},
 }
 
 -- lsps with default config
 for k, v in pairs(settings) do
-  local opts = {
-    on_attach = function(client, bufnr)
-      on_attach(client, bufnr)
-      custom_map(bufnr)
-      if client.supports_method "textDocument/signatureHelp" then
-        vim.api.nvim_create_autocmd({ "CursorHoldI" }, {
-          group = vim.api.nvim_create_augroup("LspSignature", {}),
-          callback = function()
-            vim.lsp.buf.signature_help()
-          end,
-          buffer = bufnr,
-        })
-      end
-    end,
-    capabilities = capabilities,
-  }
+	local opts = {
+		on_attach = function(client, bufnr)
+			on_attach(client, bufnr)
+			custom_map(bufnr)
+			if client.supports_method("textDocument/signatureHelp") then
+				vim.api.nvim_create_autocmd({ "CursorHoldI" }, {
+					group = vim.api.nvim_create_augroup("LspSignature", {}),
+					callback = function()
+						vim.lsp.buf.signature_help()
+					end,
+					buffer = bufnr,
+				})
+			end
+		end,
+		capabilities = capabilities,
+	}
 
-  if type(v) == "table" then
-    opts = vim.tbl_deep_extend("keep", opts, v)
-  end
-  -- assert(false, vim.inspect(opts))
+	if type(v) == "table" then
+		opts = vim.tbl_deep_extend("keep", opts, v)
+	end
+	-- assert(false, vim.inspect(opts))
 
-  if type(k) == "string" then
-    lspconfig[k].setup(opts)
-  elseif type(v) == "string" then
-    lspconfig[v].setup(opts)
-  end
+	if type(k) == "string" then
+		lspconfig[k].setup(opts)
+	elseif type(v) == "string" then
+		lspconfig[v].setup(opts)
+	end
 end
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,144 +1,146 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = { { "prettierd", "prettier" } },
-        go = { "goimports", "gofmt" },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
-    },
-    init = function()
-      -- If you want the formatexpr, here is the place to set it
-      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "clangd",
-        "cmake-language-server",
-        "css-lsp",
-        "gopls",
-        "html-lsp",
-        "lua-language-server",
-        "prettier",
-        "protolint",
-        "python-lsp-server",
-        "shfmt",
-        "stylua",
-        "typescript-language-server",
-        "vue-language-server",
-        "yapf",
-      },
-    },
-  },
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup {}
-    end,
-  },
-  {
-    "iamcco/markdown-preview.nvim",
-    ft = { "markdown" },
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-    config = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-      vim.api.nvim_create_autocmd({ "FileType" }, {
-        pattern = { "markdown" },
-        callback = function(ev)
-          vim.api.nvim_buf_set_keymap(0, "n", "<F12>", "<cmd>MarkdownPreview<CR>", { nowait = true })
-        end,
-      })
-    end,
-  },
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("nvchad.configs.lspconfig").defaults()
-      require "configs.lspconfig"
-    end,
-  },
-  {
-    "b0o/schemastore.nvim",
-    ft = { "json" },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = require "configs.treesitter",
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-      { "Marskey/telescope-sg" },
-      { "debugloop/telescope-undo.nvim" },
-    },
-    keys = {
-      { "<leader>u", "<cmd>Telescope undo<CR>", desc = "open telescope undo extensions" },
-    },
-    opts = require "configs.telescope",
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require "configs.ibl"
-    end,
-  },
-  {
-    "phaazon/hop.nvim",
-    keys = {
-      { "gl", "<cmd>HopLine<cr>", desc = "easy move", mode = { "n", "v", "x" } },
-      { "\\", "<cmd>HopChar1<cr>", desc = "global j", mode = { "n", "v", "x" } },
-    },
-    config = function()
-      require("hop").setup { keys = "etovxqpdygfblzhckisuran" }
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function(opts)
-      return require "configs.cmp"
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap",
-    ft = function()
-      return require("configs.dapconfig").filetypes()
-    end,
-    config = function()
-      require("configs.dapconfig").setup()
-    end,
-    dependencies = {
-      {
-        "rcarriga/nvim-dap-ui",
-        config = function()
-          require("configs.dapuiconfig").setup()
-        end,
-      },
-      "nvim-neotest/nvim-nio",
-      {
-        "theHamsta/nvim-dap-virtual-text",
-        config = function()
-          require("nvim-dap-virtual-text").setup()
-        end,
-      },
-    },
-  },
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { { "prettierd", "prettier" } },
+				go = { "goimports", "gofmt" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		},
+		init = function()
+			-- If you want the formatexpr, here is the place to set it
+			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"clangd",
+				"cmake-language-server",
+				"css-lsp",
+				"gopls",
+				"html-lsp",
+				"lua-language-server",
+				"prettier",
+				"protolint",
+				"python-lsp-server",
+				"shfmt",
+				"stylua",
+				"typescript-language-server",
+				"vue-language-server",
+				"yapf",
+				"emmet-ls",
+				"tailwindcss-language-server",
+			},
+		},
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		ft = { "markdown" },
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+		config = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+			vim.api.nvim_create_autocmd({ "FileType" }, {
+				pattern = { "markdown" },
+				callback = function(ev)
+					vim.api.nvim_buf_set_keymap(0, "n", "<F12>", "<cmd>MarkdownPreview<CR>", { nowait = true })
+				end,
+			})
+		end,
+	},
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("nvchad.configs.lspconfig").defaults()
+			require("configs.lspconfig")
+		end,
+	},
+	{
+		"b0o/schemastore.nvim",
+		ft = { "json" },
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = require("configs.treesitter"),
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			{ "Marskey/telescope-sg" },
+			{ "debugloop/telescope-undo.nvim" },
+		},
+		keys = {
+			{ "<leader>u", "<cmd>Telescope undo<CR>", desc = "open telescope undo extensions" },
+		},
+		opts = require("configs.telescope"),
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("configs.ibl")
+		end,
+	},
+	{
+		"phaazon/hop.nvim",
+		keys = {
+			{ "gl", "<cmd>HopLine<cr>", desc = "easy move", mode = { "n", "v", "x" } },
+			{ "\\", "<cmd>HopChar1<cr>", desc = "global j", mode = { "n", "v", "x" } },
+		},
+		config = function()
+			require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function(opts)
+			return require("configs.cmp")
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+		ft = function()
+			return require("configs.dapconfig").filetypes()
+		end,
+		config = function()
+			require("configs.dapconfig").setup()
+		end,
+		dependencies = {
+			{
+				"rcarriga/nvim-dap-ui",
+				config = function()
+					require("configs.dapuiconfig").setup()
+				end,
+			},
+			"nvim-neotest/nvim-nio",
+			{
+				"theHamsta/nvim-dap-virtual-text",
+				config = function()
+					require("nvim-dap-virtual-text").setup()
+				end,
+			},
+		},
+	},
 }
