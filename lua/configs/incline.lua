d5de9294600ea5060ebe25e5b1cadf71b2bc4123
Freeman Peng <freeman_peng@163.com>
2026-03-13 10:07:27 +0800

    pretty float navi window

--- lua/configs/incline.lua
+++ lua/configs/incline.lua
@@ -14,19 +14,21 @@ require("incline").setup({
 		local ft_icon, ft_color = devicons.get_icon_color(filename)
 		local modified = vim.bo[props.buf].modified
 		local res = {
-			ft_icon and { " ", ft_icon, "  ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
+			ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
 			" ",
-			{ filename, gui = modified and "bold,italic" or "bold" },
+			-- { filename, gui = modified and "bold,italic" or "bold" },
 			guibg = "#44406e",
 		}
 		if props.focused then
 			for _, item in ipairs(navic.get_data(props.buf) or {}) do
 				table.insert(res, {
-					{ " > ", group = "NavicSeparator" },
 					{ item.icon, group = "NavicIcons" .. item.type },
 					{ item.name, group = "NavicText" },
+					{ " > ", group = "NavicSeparator" },
 				})
 			end
+			local last = res[#res]
+			res[#res] = { last[1], last[2] }
 		end
 		table.insert(res, " ")
 		return res
