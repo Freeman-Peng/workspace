85418b4ee0d9a3d52a209db4b05799a3c6469e6d
Freeman Peng <freeman_peng@163.com>
2026-04-08 17:17:53 +0800

    incline window do not show when only icon

--- lua/configs/incline.lua
+++ lua/configs/incline.lua
@@ -8,19 +8,18 @@ require("incline").setup({
 	},
 	render = function(props)
 		local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-		if filename == "" then
-			filename = "[No Name]"
-		end
 		local ft_icon, ft_color = devicons.get_icon_color(filename)
-		local modified = vim.bo[props.buf].modified
+		local navic_data = navic.get_data(props.buf) or {}
+		if #navic_data == 0 then
+			return {}
+		end
 		local res = {
-			ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
+			ft_icon and { " ", ft_icon, "  ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
 			" ",
-			-- { filename, gui = modified and "bold,italic" or "bold" },
 			guibg = "#44406e",
 		}
 		if props.focused then
-			for _, item in ipairs(navic.get_data(props.buf) or {}) do
+			for _, item in ipairs(navic_data) do
 				table.insert(res, {
 					{ item.icon, group = "NavicIcons" .. item.type },
 					{ item.name, group = "NavicText" },
